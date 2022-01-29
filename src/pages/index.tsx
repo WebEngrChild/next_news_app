fb5abf236106b5178c8a81f68dcb686696cafc86
joe-main <qmmplusfact@gmail.com>
2022-01-29 18:52:20 +0900

    エラー対応

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,37 @@
-import MainLayout from "../layouts";
+import Head from 'next/head'
+import MainLayout from '../layouts'
 
-export default function Home() {
+// APIKEYを取得
+const API_KEY = process.env.NEWS_API_KEY
+
+export default function Home(props) {
+  // 記事を取得できているか確認
+  console.log(props.topArticles)
   return (
     <MainLayout>
+      <Head>
+        <title>Simple News</title>
+      </Head>
     </MainLayout>
   )
 }
+
+
+export const getStaticProps = async () => {
+  // 取得したい記事の数
+  const pageSize = 10 
+  // NewsAPIのトップ記事の情報を取得
+  const topRes = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${API_KEY}`
+  );
+  const topJson = await topRes.json();
+  const topArticles = topJson?.articles;
+
+  return {
+    props: {
+      topArticles,
+    },
+    // 指定時間以内のアクセスはISRを無視
+    revalidate: 60 * 10,
+  };
+};
